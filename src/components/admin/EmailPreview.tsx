388e2da70d91f5778cf0ae9df053d313f678d890
mustafa-h-lens <Mustafa@h-lens.co>
2026-03-13 00:54:29 -0700

    Updated EmailPreview.tsx

--- src/components/admin/EmailPreview.tsx
+++ src/components/admin/EmailPreview.tsx
@@ -45,7 +45,7 @@ const EmailPreview: React.FC = () => {
             textAlign: 'center'
           }}>
             <img
-              src="https://akcpkjzfhtmurtwzyzhn.supabase.co/storage/v1/object/public/images/Logo_White.png.png"
+              src="https://akcpkjzfhtmurtwzyzhn.supabase.co/storage/v1/object/public/Logo_White.png"
               alt="Half Lens"
               style={{
                 width: '120px',
